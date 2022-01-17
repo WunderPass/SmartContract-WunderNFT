95da88c7b161b5faabe8edf0917349e47ac540da
Moritz Löchner <momo.loechner@gmail.com>
2022-01-17 18:11:56 +0100

    Added Polygon and Mumbai Config

--- contracts/wunder_nft.sol
+++ contracts/wunder_nft.sol
@@ -29,11 +29,21 @@ contract WunderNFT is ERC721, VRFConsumerBase, AccessControl {
     // uint256 internal fee = 0.1 * 10 ** 18; // 0.1 LINK 
     // address internal VRFCoordinator = 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B;
     // address internal linkToken = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
-    // MAINNET
-    bytes32 internal keyHash = 0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445;
-    uint256 internal fee = 2 * 10 ** 18; // 2 LINK 
-    address internal VRFCoordinator = 0xf0d54349aDdcf704F77AE15b96510dEA15cb7952;
-    address internal linkToken = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+    // ETH Mainnet
+    // bytes32 internal keyHash = 0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445;
+    // uint256 internal fee = 2 * 10 ** 18; // 2 LINK 
+    // address internal VRFCoordinator = 0xf0d54349aDdcf704F77AE15b96510dEA15cb7952;
+    // address internal linkToken = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+    // Mumbai
+    bytes32 internal keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
+    uint256 internal fee = 0.0001 * 10 ** 18; // 0.0001 LINK 
+    address internal VRFCoordinator = 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255;
+    address internal linkToken = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    // Polygon Mainnet
+    // bytes32 internal keyHash = 0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da;
+    // uint256 internal fee = 0.0001 * 10 ** 18; // 0.0001 LINK 
+    // address internal VRFCoordinator = 0x3d2341ADb2D31f1c5530cDC622016af293177AE0;
+    // address internal linkToken = 0xb0897686c545045aFc77CF20eC7A532E3120E0F1;
 
     // Price & Edition handling
     uint public publicPrice = 1000 * 10 ** 18; // 1000 ether
