a957a082123f63bcf4ac313ec5cff9bc9a005aaa
AlpCentaur <luca.wulf@googlemail.com>
2022-01-31 16:57:58 +0000

    adding WunderSafeMath to wunder_nft.sol

---
+++ contracts/external/WunderSafeMath.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+
+pragma solidity ^0.8.9;
+
+/**
+ * @title WunderSafeMath
+ * @dev Math operations with safety checks that revert on error
+ * Combination of GnosisSafeMath and SafeMath of OpenZeppelin, renamed to WunderSafeMath to avoid conflicts
+ * WARNING: This contract should not be used with solidity compiler version  under 0.8.0, because as build on 
+ * SafeMath from openzeppelin, it relys on a function that came in with 0.8
+ * TODO from Gnosis: remove once open zeppelin update to solc 0.5.0
+ */
+library WunderSafeMath {
+    /**
+     * @dev Multiplies two numbers, reverts on overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Adds two numbers, reverts on overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+}
